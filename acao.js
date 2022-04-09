28c3fa43a4470a9f199d9a178f6e4b206366989f
Larissa Paixao <laryssapaixao9@gmail.com>
2022-04-08 23:13:21 -0300

    pedir ao usuario quantidade de cartas e embaralhar cartas

--- acao.js
+++ acao.js
@@ -1,40 +1,71 @@
-let resposta=prompt("Digite um numero par, entre 4 e 14 para escolher o numero de cartas");
-if(resposta==12){
-    let a=document.querySelector(".bloco.carta1");
-    a.classList.add("escondido");
-    let b=document.querySelector(".bloco.carta2");
-    b.classList.add("escondido");
+let Selecionou=0;
+let primeira_selecao;
+let segunda_selecao;
+let tentativas=0;
+let SoDoisClicks=0;
+let resposta;
 
-}
+do{
+resposta=prompt("Digite um numero par, entre 4 e 14 para escolher o numero de cartas");}
+while(resposta<4 || resposta>14 || resposta%2!=0)
 
-if(resposta==10){
-    let a=document.querySelector(".bloco.carta1");
-    a.classList.add("escondido");
-    let b=document.querySelector(".bloco.carta2");
-    b.classList.add("escondido");
-    let c=document.querySelector(".bloco.carta3");
-    c.classList.add("escondido");
-    let d=document.querySelector(".bloco.carta4");
-    d.classList.add("escondido");
 
-}
-
-if(resposta==8){
-    let a=document.querySelector(".bloco.carta1");
-    a.classList.add("escondido");
-    let b=document.querySelector(".bloco.carta2");
-    b.classList.add("escondido");
-    let c=document.querySelector(".bloco.carta3");
-    c.classList.add("escondido");
-    let d=document.querySelector(".bloco.carta4");
-    d.classList.add("escondido");
-    let e=document.querySelector(".bloco.carta5");
-    e.classList.add("escondido");
-    let f=document.querySelector(".bloco.carta6");
-    f.classList.add("escondido");
-}
-
-if(resposta==6){
+    if(resposta==12){
+        let a=document.querySelector(".bloco.carta1");
+        a.classList.add("escondido");
+        let b=document.querySelector(".bloco.carta2");
+        b.classList.add("escondido");
+        cartas_array2=['bloco carta1'];
+        console.log(cartas_array2);
+    }
+    
+    if(resposta==10){
+        let a=document.querySelector(".bloco.carta1");
+        a.classList.add("escondido");
+        let b=document.querySelector(".bloco.carta2");
+        b.classList.add("escondido");
+        let c=document.querySelector(".bloco.carta3");
+        c.classList.add("escondido");
+        let d=document.querySelector(".bloco.carta4");
+        d.classList.add("escondido");
+    
+    }
+    
+    if(resposta==8){
+        let a=document.querySelector(".bloco.carta1");
+        a.classList.add("escondido");
+        let b=document.querySelector(".bloco.carta2");
+        b.classList.add("escondido");
+        let c=document.querySelector(".bloco.carta3");
+        c.classList.add("escondido");
+        let d=document.querySelector(".bloco.carta4");
+        d.classList.add("escondido");
+        let e=document.querySelector(".bloco.carta5");
+        e.classList.add("escondido");
+        let f=document.querySelector(".bloco.carta6");
+        f.classList.add("escondido");
+    }
+    
+    if(resposta==6){
+        let a=document.querySelector(".bloco.carta1");
+        a.classList.add("escondido");
+        let b=document.querySelector(".bloco.carta2");
+        b.classList.add("escondido");
+        let c=document.querySelector(".bloco.carta3");
+        c.classList.add("escondido");
+        let d=document.querySelector(".bloco.carta4");
+        d.classList.add("escondido");
+        let e=document.querySelector(".bloco.carta5");
+        e.classList.add("escondido");
+        let f=document.querySelector(".bloco.carta6");
+        f.classList.add("escondido");
+        let g=document.querySelector(".bloco.carta7");
+        g.classList.add("escondido");
+        let h=document.querySelector(".bloco.carta8");
+        h.classList.add("escondido");
+    }
+    
+    if(resposta==4){
     let a=document.querySelector(".bloco.carta1");
     a.classList.add("escondido");
     let b=document.querySelector(".bloco.carta2");
@@ -51,48 +82,67 @@ if(resposta==6){
     g.classList.add("escondido");
     let h=document.querySelector(".bloco.carta8");
     h.classList.add("escondido");
-}
-
-if(resposta==4){
-let a=document.querySelector(".bloco.carta1");
-a.classList.add("escondido");
-let b=document.querySelector(".bloco.carta2");
-b.classList.add("escondido");
-let c=document.querySelector(".bloco.carta3");
-c.classList.add("escondido");
-let d=document.querySelector(".bloco.carta4");
-d.classList.add("escondido");
-let e=document.querySelector(".bloco.carta5");
-e.classList.add("escondido");
-let f=document.querySelector(".bloco.carta6");
-f.classList.add("escondido");
-let g=document.querySelector(".bloco.carta7");
-g.classList.add("escondido");
-let h=document.querySelector(".bloco.carta8");
-h.classList.add("escondido");
-let i=document.querySelector(".bloco.carta9");
-i.classList.add("escondido");
-let j=document.querySelector(".bloco.carta10");
-j.classList.add("escondido");
-}
-
-function Clicou(elemento){
-        let resposta_a=prompt("digite um numero par");
-        if(resposta_a%2==0 && resposta_a>=4 && resposta_a<=14){
-            elemento.classList.add("rosa");
-        }
-}
-
+    let i=document.querySelector(".bloco.carta9");
+    i.classList.add("escondido");
+    let j=document.querySelector(".bloco.carta10");
+    j.classList.add("escondido");
+    }
+    
+    
 function virar(){
-    console.log("quem tocou em mim?");
-    console.log(this);
-    this.classList.toggle("flip");
-}
+    this.classList.add("flip");
+
+    
+    if(Selecionou==0){
+        Selecionou=1;
+        primeira_selecao=this;
+        tentativas+=1;
+    }
+    else{
+        tentativas+=1;
+        Selecionou=0;
+        segunda_selecao=this;
+        if(primeira_selecao.dataset.jogo==segunda_selecao.dataset.jogo){
+            primeira_selecao.removeEventListener("click",virar);
+            segunda_selecao.removeEventListener("click",virar);
+            
+        }
+        else{
+            
+           setTimeout(()=>{
+                   segunda_selecao.classList.remove("flip");
+                   primeira_selecao.classList.remove("flip")},1000)
+                    
+            }
+       }
+        let total=document.getElementsByClassName("flip").length;
+        if(total==resposta){
+            alert("O JOGO ACABOU");
+            alert("Você ganhou em "+tentativas+"jogadas!");
+          
+        }
+    }
+
+
+
 
 let cartas=document.querySelectorAll(".bloco");
+//a funcao embaralhar está em parenteses porque assim 
+//ela se torna uma função iniciada imediatamente
+(function embaralhar_cartas(){
+        cartas.forEach(bloco=>{
+            let numeros=Math.floor(Math.random() * 14);
+            bloco.style.order=numeros})
+        
+})();
 //abaixo, vou colocar a função forEach 
 //ela serve p/ executar uma operação para
 //cada elemento de uma array
 // como "cartas recebeu todos os blocos, ela é um array agora"
 
-cartas.forEach(bloco => bloco.addEventListener("click",virar)); 
+cartas.forEach(bloco => {bloco.addEventListener("click",virar)}); 
+
+
+
+
+
